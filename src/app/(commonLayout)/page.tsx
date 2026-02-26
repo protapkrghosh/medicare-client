7b869a49b9e3c397b8f70434d90adb43ca7ef74c
protapkrghosh <protapg518@gmail.com>
2026-02-26 12:31:06 +0600

    change login, register position

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,7 @@
-import { authClient } from "@/lib/auth-client";
-
 export default async function Home() {
-
-   return <div></div>;
+   return (
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 xl:px-6 mt-5">
+         <h1>Home Page</h1>
+      </div>
+   );
 }
