fd831ca1672af2ec2f74515f2d9737fb49f03043
protapkrghosh <protapg518@gmail.com>
2026-02-26 12:52:22 +0600

    added banner

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,9 @@
+import HomeCarousel from "@/components/modules/home/Home";
+
 export default async function Home() {
    return (
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 xl:px-6 mt-5">
-         <h1>Home Page</h1>
+         <HomeCarousel />
       </div>
    );
 }
---
+++ src/components/modules/home/Home.tsx
@@ -0,0 +1,9 @@
+const HomeCarousel = () => {
+   return (
+      <div>
+         <h1>Home Page</h1>
+      </div>
+   );
+};
+
+export default HomeCarousel;
